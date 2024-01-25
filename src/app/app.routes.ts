0bcde5f0800865c0eb73519b3c52202267a2cd45
Joel Erni <joel.erni@hispeed.ch>
2024-01-25 16:30:07 +0100

    task 2 not done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,4 +17,8 @@ export const routes: Routes = [
     path: 'task3',
     loadComponent: () => import('./task3/task3.page').then(m => m.Task3Page)
   },
+  {
+    path: 'task4',
+    loadComponent: () => import('./task4/task4.page').then( m => m.Task4Page)
+  },
 ];
