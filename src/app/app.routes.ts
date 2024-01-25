bd172d657191a9e11c71d9e566d09437e14dd138
Joel Erni <joel.erni@hispeed.ch>
2024-01-25 10:11:25 +0100

    geolocation test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'task1',
+    loadComponent: () => import('./task1/task1.page').then( m => m.Task1Page)
+  },
 ];
--- src/app/task.ts
+++
@@ -1,7 +0,0 @@
-export interface Task {
-  id: number;
-  title: string;
-  desc: string;
-  done: boolean;
-  type: string;
-}
