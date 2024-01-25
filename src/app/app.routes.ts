f387a7a4aba2cf829bb24c434c34b9875a55f552
Joel Erni <joel.erni@hispeed.ch>
2024-01-25 16:08:38 +0100

    task 2/3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,4 +9,12 @@ export const routes: Routes = [
     path: 'task1',
     loadComponent: () => import('./task1/task1.page').then( m => m.Task1Page)
   },
+  {
+    path: 'task2',
+    loadComponent: () => import('./task2/task2.page').then( m => m.Task2Page)
+  },
+  {
+    path: 'task3',
+    loadComponent: () => import('./task3/task3.page').then(m => m.Task3Page)
+  },
 ];
--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -22,6 +22,21 @@ export const routes: Routes = [
           import('../settings/settings.page').then((m) => m.SettingsPage),
       },
       {
+        path: 'task1',
+        loadComponent: () =>
+          import('../task1/task1.page').then((m) => m.Task1Page),
+      },
+      {
+        path: 'task2',
+        loadComponent: () =>
+          import('../task2/task2.page').then((m) => m.Task2Page),
+      },
+      {
+        path: 'task3',
+        loadComponent: () =>
+          import('../task3/task3.page').then((m) => m.Task3Page),
+      },
+      {
         path: '',
         redirectTo: '/tabs/home',
         pathMatch: 'full',
